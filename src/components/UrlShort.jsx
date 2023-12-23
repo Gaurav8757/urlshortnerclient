3ce32272404b041f7ae2aab97a85a6cc57881cbe
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2023-12-23 11:50:21 +0530

    ui update

--- src/components/UrlShort.jsx
+++ src/components/UrlShort.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-
+import { toast } from 'react-toastify';
 const UrlShort = () => {
   const [inputURL, setInputURL] = useState("");
   const [shortURL, setShortURL] = useState("");
@@ -23,8 +23,10 @@ const UrlShort = () => {
 
       setShortURL(response.data.short_url);
       setIsValidURL(true);
+      toast.success('Url generated successfullly!!');
     } catch (error) {
       console.error(error);
+      toast.success('Url not generated...!!');
     }
   };
 
@@ -35,20 +37,21 @@ const UrlShort = () => {
 
   const redirectToOriginal = async () => {
     try {
-        // Make an API request to get the original URL based on the short URL
-        const response = await axios.get(`http://localhost:8000/homepage/${shortURL}`);
-        const originalURL = response.data.original_url;
-console.log(response);
-        // Redirect to the original URL
-        window.location.href = originalURL;
+      // Make an API request to get the original URL based on the short URL
+      const response = await axios.get(`http://localhost:8000/homepage/${shortURL}`);
+      const originalURL = response.data.original_url;
+      // console.log(response);
+      // Redirect to the original URL
+      window.location.href = originalURL;
+
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-};
+  };
 
   const isValidURLFormat = (url) => {
     // Use a regular expression to check if the URL is in a valid format
-    const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+    const urlRegex = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
     return urlRegex.test(url);
   };
 
@@ -79,17 +82,17 @@ console.log(response);
             <div className="w-full p-2 border border-gray-300 rounded">
               <strong>Original URL:</strong> {inputURL}
             </div>
-            <input
+            {/* <input
               className="w-full p-2 border border-gray-300 rounded"
               type="text"
               value={shortURL}
               readOnly
-            />
-           
+            /> */}
+
             <button to="#" onClick={redirectToOriginal} className="w-1/2 p-2 border border-gray-300 rounded">
-    {shortURL}
-</button>
-<button
+              {shortURL}
+            </button>
+            <button
               className="bg-green-500 text-white p-2 rounded"
               onClick={handleCopy}
             >
